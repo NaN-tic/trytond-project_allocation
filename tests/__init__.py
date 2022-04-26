5e6c331105a081aad0b5d4a4739c05dbe9a859d1
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:26:03 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part project_allocation module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from .test_project_allocation import suite
-
-__all__ = ['suite']
